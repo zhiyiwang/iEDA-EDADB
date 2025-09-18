de09dc40075efafe1d4532033b7c687b0789b37c
zhiyiwang <zhiyiwang@aliyun.com>
2025-09-18 19:26:20 +0800

    edadb macro: add cpp and update cmake

---
+++ src/database/edadb/macro.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file macro.h
+ * @brief This file contains macros to ORM from iEDA using the edadb library.
+ * @author Zhiyi Wang
+ */
+
+#include "macro.h"
+
+
+edadb::Cpp2SqlTypeTrait<idb::IdbRect>::setHasPrimKey(false); // no primary key
+edadb::Cpp2SqlTypeTrait<idb::IdbCoordinate<int32_t>>::setHasPrimKey(false); // no primary key
+
+
+
+
